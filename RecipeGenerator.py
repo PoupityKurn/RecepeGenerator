dc580ec4fa66c292c3b5ace4aa1805b85ae4d4ab
PoupityKurn <127196782+PoupityKurn@users.noreply.github.com>
2023-03-24 11:18:09 +0100

    [FEAT] POC for page RecipeGenerator.py done

--- RecipeGenerator.py
+++ RecipeGenerator.py
@@ -5,6 +5,7 @@ import boto3
 from boto3.dynamodb.conditions import Key, Attr
 from datetime import datetime
 from utils import check_password
+import logging
 
 
 class Recipe:
@@ -16,13 +17,15 @@ class Recipe:
         """
         self.dyn_resource = dyn_resource
         self.table = self.dyn_resource.Table("Recipe")
-        self.recipe = get_recipe_for_current_month(self.table, month, "Main dishes")
+        self.recipe = get_recipe_for_current_month(self.table, month, "Main dish")
+        self.dessert = get_recipe_for_current_month(self.table, month, "Dessert")
+        logging.info("The recipes have been downloaded")
 
 
 @st.cache_resource
 def get_recipe_for_current_month(_table, month, recipe_type):
     response = _table.scan(
-        FilterExpression=Attr('Month').contains(month) & Attr("Type").eq("Main dish")
+        FilterExpression=Attr('Months').contains(month) & Attr("Type").eq(recipe_type)
     )
     items = response['Items']
     return items
@@ -36,25 +39,59 @@ def display_recipe(recipe):
         st.write(recipe["Ingredients"])
 
 
+def select_random_recipe(bool_need_to_cook, recipe):
+    return random.choice(recipe) if bool_need_to_cook else None
+
+
+def display_dataframe_asking():
+    st.subheader("Select when you need to prepare a meal:")
+    number_of_lunch = pd.DataFrame([
+        {"command": "Monday", "Diner": True, "Lunch": True, "Dessert for Diner": False, "Dessert for Lunch": False},
+        {"command": "Tuesday", "Diner": True, "Lunch": True, "Dessert for Diner": False, "Dessert for Lunch": False},
+        {"command": "Wednesday", "Diner": True, "Lunch": True, "Dessert for Diner": False, "Dessert for Lunch": False},
+        {"command": "Thursday", "Diner": True, "Lunch": True, "Dessert for Diner": False, "Dessert for Lunch": False},
+        {"command": "Friday", "Diner": True, "Lunch": True, "Dessert for Diner": False, "Dessert for Lunch": False},
+        {"command": "Saturday", "Diner": False, "Lunch": False, "Dessert for Diner": True, "Dessert for Lunch": False},
+        {"command": "Sunday", "Diner": False, "Lunch": False, "Dessert for Diner": False, "Dessert for Lunch": True},
+    ])
+    df = st.experimental_data_editor(number_of_lunch)
+    return df
+
+
+def display_result_dataframe(df):
+    data = df.copy()
+    data["Diner main dish"] = data["Diner"].map(lambda x: random.choice(Recipe.recipe)["Name"] if x else None)
+    data["Launch main dish"] = data["Lunch"].map(lambda x: random.choice(Recipe.recipe)["Name"] if x else None)
+    data["Diner dessert"] = data["Dessert for Diner"].map(
+        lambda x: random.choice(Recipe.dessert)["Name"] if x else None)
+    data["Launch dessert"] = data["Dessert for Lunch"].map(
+        lambda x: random.choice(Recipe.dessert)["Name"] if x else None)
+    data = data.drop(columns=["Diner", "Lunch", "Dessert for Diner", "Dessert for Lunch"])
+
+    st.subheader("Your weekly meal planning:")
+    st.experimental_data_editor(data, width=1000)
+
+    return data
+
+
+def save_planning(data):
+    if st.button("Save this planning"):
+        data.to_parquet("planning_saved.parquet")
+        st.success("The file has been saved!")
+        logging.info("A user has saved the planning")
+
+
 if __name__ == '__main__':
+    logging.basicConfig(filename='logging.log')
+
     if check_password():
         days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-        st.write("")
-        number_of_lunch = pd.DataFrame([
-            {"command": "Monday", "Diner": True, "Lunch": False, "Dessert for Diner": False, "Dessert for Lunch": False},
-            {"command": "Tuesday", "Diner": True, "Lunch": False, "Dessert for Diner": False, "Dessert for Lunch": False},
-            {"command": "Wednesday", "Diner": True, "Lunch": False, "Dessert for Diner": False, "Dessert for Lunch": False},
-            {"command": "Thursday", "Diner": True, "Lunch": False, "Dessert for Diner": False, "Dessert for Lunch": False},
-            {"command": "Friday", "Diner": True, "Lunch": False, "Dessert for Diner": False, "Dessert for Lunch": False},
-            {"command": "Saturday", "Diner": False, "Lunch": False, "Dessert for Diner": False, "Dessert for Lunch": False},
-            {"command": "Sunday", "Diner": False, "Lunch": False, "Dessert for Diner": False, "Dessert for Lunch": False},
-        ])
-        df = st.experimental_data_editor(number_of_lunch)
+
         month = st.sidebar.slider("Change the month", 1, 12, datetime.now().month)
         dynamo_client = boto3.resource(service_name='dynamodb', region_name='eu-west-3')
         Recipe = Recipe(dynamo_client, month)
-        for day in days_of_week:
-            st.header(day)
-            # TODO: Separate dessert and main dish
-            display_recipe(random.choice(Recipe.recipe))
 
+        df = display_dataframe_asking()
+        data = display_result_dataframe(df)
+
+        save_planning(data)
